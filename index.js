9be8533b5d955decee2cee96b25fd4dd70dee40a
ansari-dot <0349ansari@gmail.com>
2025-07-21 02:36:11 +0500

    changes

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(morgan());
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://shehrity.netlify.app',
     credentials: true,
   }));
 // Routes
